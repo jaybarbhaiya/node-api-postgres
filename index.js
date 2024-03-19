1e879a70c1520268ed7c8e1fe6b0da85cbc05773
Jay Barbhaiya <jay.barbhaiya@sap.com>
2024-03-19 17:13:08 +0100

    remove error handling middleware

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 8000;
 const db = require('./queries');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
-    const error = new Error('Something went wrong');
-    next(error)
-});
-//Error handling middleware
-app.use((err, req, res, next) => {
-    console.error('Error:', err.message);
-    res.status(500).send('Internal Server Error');
-});
+// app.use((req, res, next) => {
+//     const error = new Error('Something went wrong');
+//     next(error)
+// });
+// //Error handling middleware
+// app.use((err, req, res, next) => {
+//     console.error('Error:', err.message);
+//     res.status(500).send('Internal Server Error');
+// });
 
 app.get('/', (req, res) => {
     res.json({ info: 'Node.js, Express, and Postgres API' });
