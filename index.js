cc06f2a8e43ac491435779a1bfe42dc352444453
Jay Barbhaiya <jay.barbhaiya@sap.com>
2024-03-19 13:13:16 +0100

    adding cors and error handling middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -6,6 +7,17 @@ const db = require('./queries');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+    const error = new Error('Something went wrong');
+    next(error)
+});
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message);
+    res.status(500).send('Internal Server Error');
+});
 
 app.get('/', (req, res) => {
     res.json({ info: 'Node.js, Express, and Postgres API' });
